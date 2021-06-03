9ea946d280eddf7e6dc39be8fd612425a8f6601f
Wanderson R Marques <wanderson.r.marques@gmail.com>
2021-06-03 18:56:34 -0300

    Acrescentado o footer

---
+++ includes/footer.php
@@ -0,0 +1,78 @@
+<footer class="bleezy-footer-area">
+        <div class="footer-top-area section_50">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3 col-sm-3">
+                        <div class="single-footer-widget footer-logo-widget">
+                            <div class="footer-logo">
+                                <a href="index-2.html">
+                                    <img src="assets/img/footer-logo.png" alt="footer logo" />
+                                </a>
+                            </div>
+                            <div class="footer-widget-text">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt labore et dolore magna aliqua.</p>
+                                <p>Suspendisse augue sem, iaculis id fermentum sit amet, finibus eu risus.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-3">
+                        <div class="single-footer-widget">
+                            <h3>latest post</h3>
+                            <div class="latest-post-footer clearfix">
+                                <div class="latest-post-footer-left">
+                                    <i class="fa fa-image"></i>
+                                </div>
+                                <div class="latest-post-footer-right">
+                                    <h4><a href="#">Revealed: How to set goals for you and your team</a></h4>
+                                    <p>Jan 14, 2018</p>
+                                </div>
+                            </div>
+                            <div class="latest-post-footer clearfix">
+                                <div class="latest-post-footer-left">
+                                    <i class="fa fa-briefcase"></i>
+                                </div>
+                                <div class="latest-post-footer-right">
+                                    <h4><a href="#">Five ways to improve as a business professional!</a></h4>
+                                    <p>Jan 14, 2018</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-3">
+                        <div class="single-footer-widget">
+                            <h3>main links</h3>
+                            <ul>
+                                <li><a href="#">About us</a></li>
+                                <li><a href="#">Delivery Information</a></li>
+                                <li><a href="#">Terms & Conditions</a></li>
+                                <li><a href="#">Privacy Policy</a></li>
+                                <li><a href="#">Contact us</a></li>
+                                <li><a href="#">Return Policy</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-3">
+                        <div class="single-footer-widget">
+                            <h3>Newsletter</h3>
+                            <p>Don't miss to subscribe to our news feeds, kindly fill the form below.</p>
+                            <form>
+                                <input type="email" placeholder="Subscribe">
+                                <button type="submit"><i class="fa fa-envelope-open-o"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copyright-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="copyright-right">
+                            <p>&copy; Copyright <a href="#">Themescare</a> 2018. All rights reserved</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
