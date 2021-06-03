5d35481394913e68a6f2429a1d0b84f07775a92c
Wanderson R Marques <wanderson.r.marques@gmail.com>
2021-06-03 18:49:15 -0300

    Layout acrescentado

---
+++ includes/header.php
@@ -0,0 +1,210 @@
+<header class="bleezy-header-area">
+        <div class="header-right-overlay"></div>
+        <div class="mobile-top-menu">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-sm-6 col-xs-6">
+                        <p><i class="fa fa-user"></i><a href="#">Register</a> or <a href="#">Login</a></p>
+                    </div>
+                    <div class="col-md-6 col-sm-6 col-xs-6">
+                        <div class="cart-top-menu">
+                            <div class="login dropdown">
+                                <a href="#" class="dropdown-toggle cart-icon" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                                   <i class="fa fa-shopping-bag"></i> Cart(02)
+                                </a>
+                                <div class="dropdown-menu cart-dropdown" aria-labelledby="dropdownMenu2">
+                                    <h3>Recently added item(s)</h3>
+                                    <ul>
+                                        <li>
+                                            <div class="cart-btn-product">
+                                                <a class="product-remove" href="#">
+                                                    <i class="fa fa-trash-o"></i>
+                                                </a>
+                                                <div class="cart-btn-pro-img">
+                                                    <a href="#">
+                                                        <img src="assets/img/pro.png" alt="product" />
+                                                    </a>
+                                                </div>
+                                                <div class="cart-btn-pro-cont">
+                                                    <h4><a href="#">Wireless IP Camera</a></h4>
+                                                    <span class="item-cat">
+                                                        1x$30.00
+                                                    </span>
+                                                    <span class="price">
+                                                        $30.00
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div class="cart-btn-product">
+                                                <a class="product-remove" href="#">
+                                                    <i class="fa fa-trash-o"></i>
+                                                </a>
+                                                <div class="cart-btn-pro-img">
+                                                    <a href="#">
+                                                        <img src="assets/img/pro-2.png" alt="product" />
+                                                    </a>
+                                                </div>
+                                                <div class="cart-btn-pro-cont">
+                                                    <h4><a href="#">Door Lock System</a></h4>
+                                                    <span class="item-cat">
+                                                        1x$130.00
+                                                    </span>
+                                                    <span class="price">
+                                                        $130.00
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                    <div class="cart-btn">
+                                        <a href="#" class="cart-btn-1">View Cart</a>
+                                        <a href="#" class="cart-btn-2">Checkout</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="site-logo">
+                        <a href="index-2.html">
+                            <img src="assets/img/site-logo.png" alt="site logo" />
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-9">
+                    <div class="header-right">
+                        <div class="header-right-top">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="single-top-right">
+                                        <p>Call Us: <a href="#">(+1) 828-376-0532</a></p>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="single-top-right">
+                                        <ul>
+                                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                                            <li><a href="#"><i class="fa fa-skype"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="single-top-right">
+                                        <p><i class="fa fa-user"></i> <a href="#">Register</a> or <a href="#">Login</a></p>
+                                        <div class="cart-top-menu">
+                                            <div class="login dropdown">
+                                                <a href="#" class="dropdown-toggle cart-icon" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                                                   <i class="fa fa-shopping-bag"></i> Cart(02)
+                                                </a>
+                                                <div class="dropdown-menu cart-dropdown" aria-labelledby="dropdownMenu1">
+                                                    <h3>Recently added item(s)</h3>
+                                                    <ul>
+                                                        <li>
+                                                            <div class="cart-btn-product">
+                                                                <a class="product-remove" href="#">
+                                                                    <i class="fa fa-trash-o"></i>
+                                                                </a>
+                                                                <div class="cart-btn-pro-img">
+                                                                    <a href="#">
+                                                                        <img src="assets/img/pro.png" alt="product" />
+                                                                    </a>
+                                                                </div>
+                                                                <div class="cart-btn-pro-cont">
+                                                                    <h4><a href="#">Wireless IP Camera</a></h4>
+                                                                    <span class="item-cat">
+                                                                        1x$30.00
+                                                                    </span>
+                                                                    <span class="price">
+                                                                        $30.00
+                                                                    </span>
+                                                                </div>
+                                                            </div>
+                                                        </li>
+                                                        <li>
+                                                            <div class="cart-btn-product">
+                                                                <a class="product-remove" href="#">
+                                                                    <i class="fa fa-trash-o"></i>
+                                                                </a>
+                                                                <div class="cart-btn-pro-img">
+                                                                    <a href="#">
+                                                                        <img src="assets/img/pro-2.png" alt="product" />
+                                                                    </a>
+                                                                </div>
+                                                                <div class="cart-btn-pro-cont">
+                                                                    <h4><a href="#">Door Lock System</a></h4>
+                                                                    <span class="item-cat">
+                                                                        1x$130.00
+                                                                    </span>
+                                                                    <span class="price">
+                                                                        $130.00
+                                                                    </span>
+                                                                </div>
+                                                            </div>
+                                                        </li>
+                                                    </ul>
+                                                    <div class="cart-btn">
+                                                        <a href="#" class="cart-btn-1">View Cart</a>
+                                                        <a href="#" class="cart-btn-2">Checkout</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="menu-container">
+                            <div class="row">
+                                <div class="col-md-11 col-sm-11">
+                                    <!-- Responsive Menu -->
+                                    <div class="bleezy-responsive-menu"></div>
+                                    <!-- Responsive Menu -->
+                                    <div class="mainmenu">
+                                        <nav>
+                                            <ul id="bleezy_navigation">
+                                                <li class="current-page-item"><a href="index.php">principal</a></li>
+                                                <li><a href="quem-somos.php">quem somos</a></li>
+                                                <li>
+                                                    <a href="#">serviços</a>
+                                                    <ul>
+                                                        <li><a href="service.html">para empresas</a></li>
+                                                        <li><a href="single-service.html">para condomínios</a></li>
+                                                    </ul>
+                                                </li>                                                
+                                                <li><a href="#">informativos</a>                                                   
+                                                <li><a href="contato.php">contato</a></li>
+                                            </ul>
+                                        </nav>
+                                    </div>
+                                </div>
+                                <div class="col-md-1 col-sm-1">
+                                    <div class="header-search">
+                                        <div class="search-icon">
+                                            <i class="fa fa-search"></i>
+                                        </div>
+                                        <div class="search-form">
+                                            <form>
+                                                <input type="search" placeholder="Search..." >
+                                                <button type="submit" ><i class="fa fa-search"></i></button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+  
---
+++ includes/slider.php
@@ -0,0 +1,55 @@
+<section class="bleezy-slider-area">
+        <div class="bleezy-slide">
+            <div class="bleezy-main-slide slide-item-1">
+                <div class="bleezy-main-caption">
+                    <div class="bleezy-caption-cell">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="slider-text">
+                                        <h2>the most successfull security <span>agency</span></h2>
+                                        <p>Nunc accumsan metus quis metus. Sed luctus. Mauris eu enim quisque dignissim nequesudm consectetuer dapibus wn eu leo integer varius erat.</p>
+                                        <a href="#" class="bleezy-btn">start a project</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bleezy-main-slide slide-item-2">
+                <div class="bleezy-main-caption">
+                    <div class="bleezy-caption-cell">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="slider-text">
+                                        <h2>Premium Security Services Trusted <span> by Millions</span></h2>
+                                        <p>Nunc accumsan metus quis metus. Sed luctus. Mauris eu enim quisque dignissim nequesudm consectetuer dapibus wn eu leo integer varius erat.</p>
+                                        <a href="#" class="bleezy-btn">learn more</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bleezy-main-slide slide-item-3">
+                <div class="bleezy-main-caption">
+                    <div class="bleezy-caption-cell">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="slider-text">
+                                        <h2>Premium Security Services Trusted<span> by Millions</span></h2>
+                                        <p>Nunc accumsan metus quis metus. Sed luctus. Mauris eu enim quisque dignissim nequesudm consectetuer dapibus wn eu leo integer varius erat.</p>
+                                        <a href="#" class="bleezy-btn">learn more</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
